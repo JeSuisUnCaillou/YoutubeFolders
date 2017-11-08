a0744e5c07f6917cdb6321b7307fb844bfebe18d
Caillou <igorlacroute@gmail.com>
2017-11-08 18:37:02 +0100

    refacto some code into get_all_channels_in_page

--- content.js
+++ content.js
@@ -2,17 +2,14 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
 		//Get all channels ids
-		var href_substring = '/channel/'
-		var channels = $("a[href^='"+href_substring+"']").map(function() {
-			return $(this).attr('href').substr(href_substring.length);
-		})
-		//console.log(channels)
+		var channels = get_all_channels_in_page()
 		
 		send_to_back("save_data", channels)
 		send_to_back("get_data")
 		
     } else if (request.message === "data_saved"){
 		console.log("data saved !")
+		
 	} else if (request.message === "data"){
 		console.log(request.data)
 	}
@@ -25,4 +22,13 @@ var send_to_back = function(message, data){
 		"message": message,
 		"data": data
 	})
+}
+
+//Get all channels ids in page
+var get_all_channels_in_page = function(){
+	var href_substring = '/channel/'
+	var channels = $("a[href^='"+href_substring+"']").map(function() {
+		return $(this).attr('href').substr(href_substring.length);
+	})
+	return channels
 }
