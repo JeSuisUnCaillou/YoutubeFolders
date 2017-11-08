5958790b672d5054c93f39ee183e8c83f21b7e31
Caillou <igorlacroute@gmail.com>
2017-11-08 17:55:39 +0100

    get the channels ids

--- content.js
+++ content.js
@@ -1,10 +1,16 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-		var channels = $("a[href^='/channel/']")
+		//Get all channels ids
+		var href_substring = '/channel/'
+		var channels = $("a[href^='"+href_substring+"']").map(function() {
+			return $(this).attr('href').substr(href_substring.length);
+		})
 		console.log(channels.length + " channels")
-		var firstHref = channels.eq(0).attr("href")
-		console.log(firstHref);
+		console.log(channels)
+		
+		//var firstHref = channels.eq(0).attr("href")
+		//console.log(firstHref);
 	  
 		//chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://www.youtube.com" + firstHref});
     }
