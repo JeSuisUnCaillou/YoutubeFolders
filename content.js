9ba1fada0829b16dde0963441bdac42ca8a6decb
Caillou <igorlacroute@gmail.com>
2017-11-08 17:41:43 +0100

    make it count channels in page

--- content.js
+++ content.js
@@ -1,11 +1,12 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='/channel/']").eq(0).attr("href");
-
-      console.log(firstHref);
+		var channels = $("a[href^='/channel/']")
+		console.log(channels.length + " channels")
+		var firstHref = channels.eq(0).attr("href")
+		console.log(firstHref);
 	  
-	  chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://www.youtube.com" + firstHref});
+		//chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://www.youtube.com" + firstHref});
     }
   }
 );
