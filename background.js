66b243c82cd382caf1a1a96ca88d61af2bd836b8
Caillou <igorlacroute@gmail.com>
2017-11-08 18:34:07 +0100

    Save and get data in chrome sync storage

--- background.js
+++ background.js
@@ -1,17 +1,37 @@
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+  send_to_current_page("clicked_browser_action")
 });
 
-//called to open new tab
+//Messages received from content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+	//called to open new tab
     if( request.message === "open_new_tab" ) {
       chrome.tabs.create({"url": request.url});
     }
+	//called save some data
+	else if( request.message === "save_data" ) {
+	  chrome.storage.sync.set({ "YoutubeFolders": request.data }, function(){
+	     // Send a message to the active tab if sucessful save
+		  send_to_current_page("data_saved")
+	  });
+    }
+	//called to get some data
+	else if( request.message === "get_data" ) {
+		chrome.storage.sync.get("YoutubeFolders", function(items){
+			send_to_current_page("data", items["YoutubeFolders"])
+		});
+	}
   }
 );
+
+// Send a message to the active tab. Data is not mandatory.
+var send_to_current_page = function(message, data){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	var activeTab = tabs[0];
+	chrome.tabs.sendMessage(activeTab.id, {"message": message, "data": data});
+  });
+}
--- content.js
+++ content.js
@@ -6,13 +6,23 @@ chrome.runtime.onMessage.addListener(
 		var channels = $("a[href^='"+href_substring+"']").map(function() {
 			return $(this).attr('href').substr(href_substring.length);
 		})
-		console.log(channels.length + " channels")
-		console.log(channels)
+		//console.log(channels)
 		
-		//var firstHref = channels.eq(0).attr("href")
-		//console.log(firstHref);
-	  
-		//chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://www.youtube.com" + firstHref});
-    }
+		send_to_back("save_data", channels)
+		send_to_back("get_data")
+		
+    } else if (request.message === "data_saved"){
+		console.log("data saved !")
+	} else if (request.message === "data"){
+		console.log(request.data)
+	}
   }
 );
+
+//Send messages to background.js
+var send_to_back = function(message, data){
+	chrome.runtime.sendMessage({
+		"message": message,
+		"data": data
+	})
+}
